0845be342c2df7b351cb90da2eef03f49e30e43f
nl-trantrungdong <trandong2805@gmail.com>
2024-05-27 15:10:24 +0700

    3

--- src/main/java/DSA.java
+++ src/main/java/DSA.java
@@ -129,11 +129,8 @@ public class DSA {
 
     // Hàm xác minh chữ ký
     public boolean verify(String messagehash, String signature, PublicKey publicKey) {
-        try {
-            // Chuyển đổi chuỗi Base64 thành mảng byte
-            byte[] signatureBytes = Base64.getDecoder().decode(signature);
-            Signature sig = null;
-            sig = Signature.getInstance("SHA256withDSA");
+        byte[] keyEncoded = publicKey.getEncoded();
+        return Base64.getEncoder().encodeToString(keyEncoded);
             sig.initVerify(publicKey);
             sig.update(messagehash.getBytes());
             return sig.verify(signatureBytes);
